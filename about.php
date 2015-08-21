6cd8c9ee80a45bfe33b80a9390627bc666148328
Wolf Devil <ahmedsafadii@gmail.com>
2015-08-21 14:28:11 +0300

    Pages Files

---
+++ about.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link href="css/fonts.css" rel="stylesheet" type="text/css"/>
+        <title>Riot Project v2 - About Us</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/custom.css" rel="stylesheet">
+        <link href="css/about.css" rel="stylesheet" type="text/css"/>
+
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <!--load font awesome-->
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">Riot Project v2</a>
+                </div>
+                <!-- Search -->
+                <form action="result.php" method="get" class="navbar-form navbar-left hidden-sm hidden-xs" role="search">
+                    <div class="form-group">
+                        <input class="form-control" style="width:250px;" placeholder="Item Name ..." type="text">
+                    </div>
+                    <button type="submit" class="btn btn-danger">Search</button>
+                </form>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">                  
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="index.php">Home</a>
+                        </li>
+                        <li>
+                            <a href="game.php">Game</a>
+                        </li>
+                        <li>
+                            <a href="score.php">High Score</a>
+                        </li>      
+                        <li>
+                            <a href="export.php">Export Data</a>
+                        </li>
+                        <li>
+                            <a href="about.php">About</a>
+                        </li>                       
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+
+        <!-- Page Content -->
+        <div class="container topmargin">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h1>About Us</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6" style="background-color: #9D9D9D;">
+                    <div class="widget-head-color-box navy-bg p-lg text-center">
+                        <div class="m-b-md">
+                            <h2 style="color:black;" class="font-bold no-margins">
+                                Ahmed Safadi
+                            </h2>
+                            <Br />
+                            <small>Web Designer</small>
+                        </div>
+                        <img  width="200" height="200"  class="img-circle" src="a4.jpg" class="img-circle circle-border m-b-md" alt="profile">
+                        <div style="color:black;margin-top:15px;">
+                            <span>20-01-1994</span> |
+                            <span>Palestine - Gaza</span> |
+                            <span>+972598195871</span>
+                        </div>
+                    </div>
+                    <div class="widget-text-box">
+                        <div class="text-center">
+                            <div class="social">
+                                <ul>
+                                    <li><a href="#"><i class="fa fa-lg fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-lg fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-lg fa-google-plus"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-lg fa-github"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-lg fa-instagram"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-lg fa-dropbox"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-lg fa-skype"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-lg fa-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+                <hr class="hidden-lg" />
+                <div class="col-lg-6" style="background-color:#C9302C;">
+                    <div class="widget-head-color-box navy-bg p-lg text-center">
+                        <div class="m-b-md">
+                            <h2 style="color:black;" class="font-bold no-margins">
+                                Tino Fuhrmann
+                            </h2>
+                            <Br />
+                            <small>Web Programmer</small>
+                        </div>
+                        <img  width="200" height="200"  class="img-circle" src="a4.jpg" class="img-circle circle-border m-b-md" alt="profile">
+                        <div style="color:black;margin-top:15px;">
+                            <span>dd-mm-yyy</span> |
+                            <span>German - City</span> |
+                            <span>+000000000000</span>
+                        </div>
+                    </div>
+                    <div class="widget-text-box">
+                        <div class="text-center">
+                            <div class="social">
+                                <ul>
+                                    <li><a href="#"><i class="fa fa-lg fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-lg fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-lg fa-google-plus"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-lg fa-github"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-lg fa-instagram"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-lg fa-dropbox"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-lg fa-skype"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-lg fa-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>                 
+            </div>
+            <!-- Footer -->
+            <footer>
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <hr />
+                        <p>Copyright &copy; Riot Project v2</p>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </footer>
+
+        </div>
+        <!-- /.container -->
+
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+        <script>
+            $('[data-toggle="popover"]').popover({trigger: 'hover', 'placement': 'top'});
+        </script>
+    </body>
+
+</html>
---
+++ item.php
@@ -0,0 +1,271 @@
+<?php
+include 'db.php';
+$itemid = strip_tags(htmlspecialchars($_GET['id']));
+$result = $mysqli->query("SELECT * FROM `511` WHERE `id` = $itemid ");
+$row = $result->fetch_assoc();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link href="css/fonts.css" rel="stylesheet" type="text/css"/>
+        <title>Riot Project v2 - <?php echo $row['name']; ?></title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/custom.css" rel="stylesheet">
+        <link href="css/game.css" rel="stylesheet" type="text/css"/>
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+
+    <body>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">Riot Project v2</a>
+                </div>
+                <!-- Search -->
+                <form action="result.php" method="get" class="navbar-form navbar-left hidden-sm hidden-xs" role="search">
+                    <div class="form-group">
+                        <input class="form-control" style="width:250px;" placeholder="Item Name ..." type="text">
+                    </div>
+                    <button type="submit" class="btn btn-danger">Search</button>
+                </form>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">                  
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active">
+                            <a href="index.php">Home</a>
+                        </li>
+                        <li>
+                            <a href="game.php">Game</a>
+                        </li>
+                        <li>
+                            <a href="score.php">High Score</a>
+                        </li>      
+                        <li>
+                            <a href="export.php">Export Data</a>
+                        </li>
+                        <li>
+                            <a href="about.php">About</a>
+                        </li>                       
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+
+        <!-- Page Content -->
+        <div class="container topmargin">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h1><?php echo $row['name']; ?></h1>
+                    <p><?php
+                    $items = file_get_contents("item.json");
+                    $getItem = json_decode($items, true);
+                    foreach ($getItem['data'] as $key => $val) {
+                    if ($key == $itemid) {
+                        echo $val['plaintext'];
+                    }
+                    }
+                    ?></p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 text-center">
+                    <div class="patchtitle"></div>
+                    <div class="box">
+                        <div class="pickrate">Pick Rate</div>
+                        <div class="threerate">
+                            <div class="normalpick">
+                                <div class="normaltitle">Normal</div>
+                                <div class="percentgenormal">99.22%</div>
+                            </div>
+                            <div class="arrowpick">
+                                <div class="arrowdown"></div>
+                            </div>
+                            <div class="rankedpick">
+                                <div class="rankedtitle">Ranked</div>
+                                <div class="percentgeranked">2.22%</div>
+                            </div>                                    
+                        </div>
+                    </div> 
+                    <div class="box">
+                        <div class="pickrate">Pick Rate</div>
+                        <div class="threerate">
+                            <div class="normalpick">
+                                <div class="normaltitle">Normal</div>
+                                <div class="percentgenormal">99.22%</div>
+                            </div>
+                            <div class="arrowpick">
+                                <div class="arrowdown"></div>
+                            </div>
+                            <div class="rankedpick">
+                                <div class="rankedtitle">Ranked</div>
+                                <div class="percentgeranked">2.22%</div>
+                            </div>                                    
+                        </div>
+                    </div>   
+                    <div class="box">
+                        <div class="pickrate">Pick Rate</div>
+                        <div class="threerate">
+                            <div class="normalpick">
+                                <div class="normaltitle">Normal</div>
+                                <div class="percentgenormal">99.22%</div>
+                            </div>
+                            <div class="arrowpick">
+                                <div class="arrowdown"></div>
+                            </div>
+                            <div class="rankedpick">
+                                <div class="rankedtitle">Ranked</div>
+                                <div class="percentgeranked">2.22%</div>
+                            </div>                                    
+                        </div>
+                    </div>                    
+                </div>
+                <div class="col-lg-6 text-center">
+                    <div class="requireitems">
+                        <a href="#"><img class="img-circle" src="item/<?php echo $itemid; ?>.png" alt=""/></a>
+                    </div>
+                    <?php
+                    foreach ($getItem['data'] as $key => $val) {
+                        if ($key == $itemid) {
+                            echo "<div class=\"requireitems\">";
+                            if (isset($val['from'])) {
+                                foreach ($val['from'] as $ky) {
+                                    foreach ($getItem['data'] as $keyy => $vall) {
+                                        if ($keyy == $ky) {
+                                            echo "<a class=\"mainitems\" href=\"#\" title=\"{$vall['name']}\" data-toggle=\"popover\" data-placement=\"top\" data-content=\"{$vall['plaintext']}\"><img class=\"img-circle\" src=\"item/$ky.png\" alt=\"\"/></a>";
+                                        }
+                                    }
+                                }
+                            }
+                            echo "</div>";
+                        }
+                    }
+                    ?> 
+                    <div class="table-responsive" style="margin-top:10px;">
+                        <table class="table">
+                            <thead  >
+                                <tr>
+                                    <th class="text-center">Patch/Type</th>
+                                    <th class="text-center">Winrate Normal/Ranked</th>
+                                    <th class="text-center">Picrate Normal/Ranked</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="text-center">Patch 5.11</td>
+                                    <td class="text-center">100%/100%</td>
+                                    <td class="text-center">100%/100%</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-center">Patch 5.14</td>
+                                    <td class="text-center">100%/100%</td>
+                                    <td class="text-center">100%/100%</td>
+                                </tr>                            
+                            </tbody>
+                        </table>
+                    </div>                    
+                </div>
+                <div class="col-lg-3 text-center">
+                    <div class="patchtitle2"></div>
+                    <div class="box">
+                        <div class="pickrate">Pick Rate</div>
+                        <div class="threerate">
+                            <div class="normalpick">
+                                <div class="normaltitle">Normal</div>
+                                <div class="percentgenormal">99.22%</div>
+                            </div>
+                            <div class="arrowpick">
+                                <div class="arrowdown"></div>
+                            </div>
+                            <div class="rankedpick">
+                                <div class="rankedtitle">Ranked</div>
+                                <div class="percentgeranked">2.22%</div>
+                            </div>                                    
+                        </div>
+                    </div> 
+                    <div class="box">
+                        <div class="pickrate">Pick Rate</div>
+                        <div class="threerate">
+                            <div class="normalpick">
+                                <div class="normaltitle">Normal</div>
+                                <div class="percentgenormal">99.22%</div>
+                            </div>
+                            <div class="arrowpick">
+                                <div class="arrowdown"></div>
+                            </div>
+                            <div class="rankedpick">
+                                <div class="rankedtitle">Ranked</div>
+                                <div class="percentgeranked">2.22%</div>
+                            </div>                                    
+                        </div>
+                    </div> 
+                    <div class="box">
+                        <div class="pickrate">Pick Rate</div>
+                        <div class="threerate">
+                            <div class="normalpick">
+                                <div class="normaltitle">Normal</div>
+                                <div class="percentgenormal">99.22%</div>
+                            </div>
+                            <div class="arrowpick">
+                                <div class="arrowdown"></div>
+                            </div>
+                            <div class="rankedpick">
+                                <div class="rankedtitle">Ranked</div>
+                                <div class="percentgeranked">2.22%</div>
+                            </div>                                    
+                        </div>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+        <!-- /.footer -->
+        <div class="container">
+            <hr>
+            <!-- Footer -->
+            <footer>
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <p>Copyright &copy; Riot Project v2</p>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </footer>
+
+        </div>
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+        <script>
+            $('[data-toggle="popover"]').popover({trigger: 'hover', 'placement': 'top'});
+        </script>
+    </body>
+
+</html>
