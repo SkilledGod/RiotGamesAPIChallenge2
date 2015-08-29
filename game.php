c5075068a6bf09f9c50ae0eb58826521285e04b9
Tino Fuhrmann <tino-fuhrmann@web.de>
2015-08-29 20:19:29 +0200

    Add link to Glyphicons and remove old databases

--- game.php
+++ game.php
@@ -691,7 +691,7 @@ $search = $mysqli->query("SELECT * FROM `ap_items`");
                 <div class="row">
                     <div class="col-lg-12 text-center">
                         <hr />
-                        <p>Project Name isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends &copy; Riot Games, Inc.</p>
+                        <p>Project Name isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends &copy; Riot Games, Inc.<br><a href="http://glyphicons.com/">Glyphicons</a></p>
                     </div>
                 </div>
                 <!-- /.row -->
